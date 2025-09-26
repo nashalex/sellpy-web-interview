59a0b55e6533f454d552e2d0920ef6decccb9048
Alex Nash <46897024+nashalex@users.noreply.github.com>
2025-09-26 20:26:50 +0200

    Store todo's in a local variable on the backend, and setup a query structure to facilitate the optional tasks (in theory).

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,34 @@
 import express from 'express'
 import cors from 'cors'
 
+let todos = {
+  '0000000001': {
+    id: '0000000001',
+    title: 'First List',
+    todos: ['First todo of first list!'],
+  },
+  '0000000002': {
+    id: '0000000002',
+    title: 'Second List',
+    todos: ['First todo of second list!'],
+  },
+}
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
 const PORT = 3001
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
+app.get('/', (req, res) => {
+  console.log(req.query)
+  switch (req.query.type) {
+    case 'get':
+      res.send(todos)
+      break
+    case 'set':
+    default:
+      console.log(`ERROR: Unsupported request type: ${req.params.type}`)
+  }
+})
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
--- frontend/src/todos/components/TodoLists.jsx
+++ frontend/src/todos/components/TodoLists.jsx
@@ -11,25 +11,7 @@ import {
 import ReceiptIcon from '@mui/icons-material/Receipt'
 import { TodoListForm } from './TodoListForm'
 
-// Simulate network
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-
-const fetchTodoLists = () => {
-  return sleep(1000).then(() =>
-    Promise.resolve({
-      '0000000001': {
-        id: '0000000001',
-        title: 'First List',
-        todos: ['First todo of first list!'],
-      },
-      '0000000002': {
-        id: '0000000002',
-        title: 'Second List',
-        todos: ['First todo of second list!'],
-      },
-    })
-  )
-}
+const fetchTodoLists = () => fetch('http://localhost:3001?type=get').then((todos) => todos.json())
 
 export const TodoLists = ({ style }) => {
   const [todoLists, setTodoLists] = useState({})
